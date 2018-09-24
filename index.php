91c22fde5cb74d80f9708b725db79cb242823b9e
briandlt <brianesparzadlt4@gmail.com>
2018-09-24 11:12:41 -0500

    inicio del proyecto

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tienda Deportiva</title>
+    <link href="https://fonts.googleapis.com/css?family=Asap+Condensed|Lobster" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="contenedor">
+        <header>
+            <div class="logo">
+                <a href="#">SPORT-SHOP</a>
+            </div>
+
+            
+            <div class="navbar">
+                <a href="#" class="menu"><img src="./iconos/menu-de-lineas.png" alt="menu"></a>
+                <nav class="linksNav">
+                    <a href="#">Inicio</a>
+                    <a href="#">Ingresar</a>
+                    <a href="#">Categorias</a>
+                    <a href="#">Contacto</a>
+                    <a href="#">Carro</a>
+                </nav>
+            </div>
+        </header>
+
+
+        <div class="contenido">
+
+        </div>
+
+        <footer>
+            <div class="derechos">
+                <p>Brian Esparza De La Torre - Todos los derechos reservados <?php echo date("Y"); ?> &copy;</p>
+            </div>
+            <div class="sociales">
+                <a href="#"><img src="./iconos/facebook.png" alt="facebook"></a>
+                <a href="#"><img src="./iconos/gorjeo.png" alt="tweeter"></a>
+                <a href="#"><img src="./iconos/google-plus.png" alt="google-plus"></a>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
